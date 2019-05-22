dce012a3770b81b6e0d3924486815e042200aa84
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2019-05-22 20:59:23 +0500

    Implemented Traveller's Boots MK4

--- src/main/java/com/denismasterherobrine/travellersboots/items/armor/travellersboots.java
+++ src/main/java/com/denismasterherobrine/travellersboots/items/armor/travellersboots.java
@@ -14,16 +14,16 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class travellersboots extends ItemArmor
 {
-
     public travellersboots(String name, ItemArmor.ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn)
     {
         super(materialIn, renderIndexIn, equipmentSlotIn);
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         }
-    public static ItemArmor.ArmorMaterial MKIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk1", "travellersboots:bootsmki", 4, new int[]{0, 0, 0, 0}, 4, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.LEATHER));
-    public static ItemArmor.ArmorMaterial MKIIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk2", "travellersboots:bootsmkii", 8, new int[]{0, 0, 0, 1}, 8, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.GOLD_INGOT));
-    public static ItemArmor.ArmorMaterial MKIIIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk3", "travellersboots:bootsmkiii", 12, new int[]{0, 0, 0, 2}, 16, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.DIAMOND));
+    public static ItemArmor.ArmorMaterial MKIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk1", "travellersboots:bootsmki", 4, new int[]{1, 0, 0, 0}, 4, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.LEATHER));
+    public static ItemArmor.ArmorMaterial MKIIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk2", "travellersboots:bootsmkii", 8, new int[]{2, 0, 0, 0}, 8, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.GOLD_INGOT));
+    public static ItemArmor.ArmorMaterial MKIIIMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk3", "travellersboots:bootsmkiii", 12, new int[]{4, 0, 0, 0}, 12, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.DIAMOND));
+    public static ItemArmor.ArmorMaterial MKIVMaterial = EnumHelper.addArmorMaterial("travellersboots:boots_mk4", "travellersboots:bootsmkiv", 16, new int[]{6, 0, 0, 0}, 16, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.0F).setRepairItem(new ItemStack(Items.EMERALD));
 
     @Override
     public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
@@ -32,12 +32,17 @@ public class travellersboots extends ItemArmor
                 player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 0));
             }
             if (player.inventory.armorItemInSlot(0).getItem() == ItemRegistry.travellersbootsmkii) {
-                player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 0));
-                    player.stepHeight = 1.25f;
+                player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 1));
+                player.stepHeight = 1.25f;
             }
             if (player.inventory.armorItemInSlot(0).getItem() == ItemRegistry.travellersbootsmkiii) {
-                player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 1));
-                    player.stepHeight = 1.25f;
+                player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 2));
+                player.stepHeight = 1.25f;
+            }
+            if (player.inventory.armorItemInSlot(0).getItem() == ItemRegistry.travellersbootsmkiv) {
+                player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 1, 3));
+                player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 1, 1));
+                player.stepHeight = 1.25f;
             }
         }
     }
--- src/main/java/com/denismasterherobrine/travellersboots/misc/ItemRegistry.java
+++ src/main/java/com/denismasterherobrine/travellersboots/misc/ItemRegistry.java
@@ -9,20 +9,20 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import static com.denismasterherobrine.travellersboots.items.armor.travellersboots.MKIIIMaterial;
-import static com.denismasterherobrine.travellersboots.items.armor.travellersboots.MKIIMaterial;
-import static com.denismasterherobrine.travellersboots.items.armor.travellersboots.MKIMaterial;
+import static com.denismasterherobrine.travellersboots.items.armor.travellersboots.*;
 
 public class ItemRegistry {
     public static Item travellersbootsmki = new travellersboots("bootsMKI", MKIMaterial, 1,EntityEquipmentSlot.FEET);
     public static Item travellersbootsmkii = new travellersboots("bootsMKII", MKIIMaterial, 1,EntityEquipmentSlot.FEET);
     public static Item travellersbootsmkiii = new travellersboots("bootsMKIII", MKIIIMaterial, 1,EntityEquipmentSlot.FEET);
+    public static Item travellersbootsmkiv = new travellersboots("bootsMKIV", MKIVMaterial, 1,EntityEquipmentSlot.FEET);
 
     public static void register()
     {
         registerItems(travellersbootsmki);
         registerItems(travellersbootsmkii);
         registerItems(travellersbootsmkiii);
+        registerItems(travellersbootsmkiv);
     }
 
     @SideOnly(Side.CLIENT)
@@ -31,6 +31,7 @@ public class ItemRegistry {
         registerItemsRender(travellersbootsmki);
         registerItemsRender(travellersbootsmkii);
         registerItemsRender(travellersbootsmkiii);
+        registerItemsRender(travellersbootsmkiv);
     }
 
     private static void registerItems(Item item)
