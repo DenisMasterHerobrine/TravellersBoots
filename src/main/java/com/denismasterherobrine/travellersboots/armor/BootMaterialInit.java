65fb54e1a2bba458f7e55f258841a9131dd351d5
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2019-07-06 14:14:26 +0500

    Fix crsah, when repairing items in anvil, version bump

--- src/main/java/com/denismasterherobrine/travellersboots/armor/BootMaterialInit.java
+++ src/main/java/com/denismasterherobrine/travellersboots/armor/BootMaterialInit.java
@@ -3,6 +3,8 @@ package com.denismasterherobrine.travellersboots.armor;
 import com.denismasterherobrine.travellersboots.TravellersBoots;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
@@ -10,21 +12,23 @@ import net.minecraft.util.SoundEvent;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 public enum BootMaterialInit implements IArmorMaterial {
-    travellersbootsmki("travellersbootsmki", 50, new int[] {1, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f),
-    travellersbootsmkii("travellersbootsmkii", 100, new int[] {2, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f),
-    travellersbootsmkiii("travellersbootsmkiii", 150, new int[] {4, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f),
-    travellersbootsmkiv("travellersbootsmkiv", 300, new int[] {6, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f);
+    travellersbootsmki("travellersbootsmki", 50, new int[] {1, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.LEATHER),
+    travellersbootsmkii("travellersbootsmkii", 100, new int[] {2, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.GOLD_INGOT),
+    travellersbootsmkiii("travellersbootsmkiii", 150, new int[] {4, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.DIAMOND),
+    travellersbootsmkiv("travellersbootsmkiv", 300, new int[] {6, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.EMERALD);
     private String name, equipSound;
     private int durability, enchantability;
     private final int[] damageReductionAmountArray;
     private float toughness;
-    private BootMaterialInit(String name, int durability, int[] p_i48533_5_, int enchantability, String equipSound, float toughness) {
+    private Item repairMaterial;
+    BootMaterialInit(String name, int durability, int[] p_i48533_5_, int enchantability, String equipSound, float toughness, Item repairMaterial) {
         this.name = name;
         this.equipSound = equipSound;
         this.durability = durability;
         this.enchantability = enchantability;
         this.damageReductionAmountArray = p_i48533_5_;
         this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
     }
     @Override
     public int getDamageReductionAmount(EquipmentSlotType slotIn) {
@@ -45,7 +49,7 @@ public enum BootMaterialInit implements IArmorMaterial {
     }
     @Override
     public Ingredient getRepairMaterial() {
-        return null;
+        return Ingredient.fromItems(repairMaterial);
     }
     @Override
     public SoundEvent getSoundEvent() {
