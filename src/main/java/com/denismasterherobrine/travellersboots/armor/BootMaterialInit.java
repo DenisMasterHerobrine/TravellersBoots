18b787f0bff93eb839446e39c4f1552fd0fc2344
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2020-08-31 00:49:03 +0500

    1.16.2 update

--- src/main/java/com/denismasterherobrine/travellersboots/armor/BootMaterialInit.java
+++ src/main/java/com/denismasterherobrine/travellersboots/armor/BootMaterialInit.java
@@ -12,16 +12,18 @@ import net.minecraft.util.SoundEvent;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 public enum BootMaterialInit implements IArmorMaterial {
-    travellersbootsmki("travellersbootsmki", 50, new int[] {1, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.LEATHER),
-    travellersbootsmkii("travellersbootsmkii", 100, new int[] {2, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.GOLD_INGOT),
-    travellersbootsmkiii("travellersbootsmkiii", 150, new int[] {4, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.DIAMOND),
-    travellersbootsmkiv("travellersbootsmkiv", 300, new int[] {6, 0, 0, 0}, 25, "item.armor.equip_leather", 0.0f, Items.EMERALD);
+    travellersbootsmki("travellersbootsmki", 50, new int[] {1, 0, 0, 0}, 25, 0, "item.armor.equip_leather", 0.0f, Items.LEATHER),
+    travellersbootsmkii("travellersbootsmkii", 100, new int[] {2, 0, 0, 0}, 25, 0, "item.armor.equip_leather", 0.0f, Items.GOLD_INGOT),
+    travellersbootsmkiii("travellersbootsmkiii", 150, new int[] {4, 0, 0, 0}, 25, 0, "item.armor.equip_leather", 0.0f, Items.DIAMOND),
+    travellersbootsmkiv("travellersbootsmkiv", 300, new int[] {6, 0, 0, 0}, 25, 0, "item.armor.equip_leather", 0.0f, Items.EMERALD);
     private String name, equipSound;
     private int durability, enchantability;
     private final int[] damageReductionAmountArray;
     private float toughness;
     private Item repairMaterial;
-    BootMaterialInit(String name, int durability, int[] p_i48533_5_, int enchantability, String equipSound, float toughness, Item repairMaterial) {
+    private final float knockbackResistance;
+
+    BootMaterialInit(String name, int durability, int[] p_i48533_5_, int enchantability, int knockbackResistance, String equipSound, float toughness, Item repairMaterial) {
         this.name = name;
         this.equipSound = equipSound;
         this.durability = durability;
@@ -29,32 +31,40 @@ public enum BootMaterialInit implements IArmorMaterial {
         this.damageReductionAmountArray = p_i48533_5_;
         this.toughness = toughness;
         this.repairMaterial = repairMaterial;
+        this.knockbackResistance = knockbackResistance;
     }
+
     @Override
     public int getDamageReductionAmount(EquipmentSlotType slotIn) {
         return this.damageReductionAmountArray[slotIn.getIndex()];
     }
+
     @ParametersAreNonnullByDefault
     @Override
     public int getDurability(EquipmentSlotType arg0) {
         return this.durability;
     }
+
     @Override
     public int getEnchantability() {
         return this.enchantability;
     }
+
     @Override
     public String getName() {
         return TravellersBoots.MODID+":"+this.name;
     }
+
     @Override
     public Ingredient getRepairMaterial() {
         return Ingredient.fromItems(repairMaterial);
     }
+
     @Override
     public SoundEvent getSoundEvent() {
         return new SoundEvent(new ResourceLocation(equipSound));
     }
+
     @Override
     public float getToughness() {
         return this.toughness;
@@ -62,6 +72,6 @@ public enum BootMaterialInit implements IArmorMaterial {
 
     @Override
     public float func_230304_f_() {
-        return 0; // Wait, what's this?
+        return this.knockbackResistance;
     }
 }
